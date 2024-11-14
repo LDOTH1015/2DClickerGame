8a20ec6b61e1c3436d4a1babc130fc01ccf16f16
OTH <112844098+LDOTH1015@users.noreply.github.com>
2024-11-14 11:26:29 +0900

    Feat : 파티클 시스템

--- Assets/Scripts/Button/ClickButton.cs
+++ Assets/Scripts/Button/ClickButton.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class ClickButton : MonoBehaviour
 {
+    public ParticleSystem effectParticle;
     void OnMouseDown()
     {
         GameManager.Instance.AddScore();
+        if (effectParticle != null)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = Camera.main.nearClipPlane;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            ParticleSystem particle = Instantiate(effectParticle, worldPosition, Quaternion.identity);
+
+            Destroy(particle.gameObject, 0.5f);
+        }
     }
 }
